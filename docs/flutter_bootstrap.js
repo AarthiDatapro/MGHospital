b4ceb9c65c3c0cc7042dc425837b8eba167f43d9
AarthiDatapro <aarthi@datapro.in>
2025-09-18 11:39:50 +0530

    Version EEE with web build for IOS

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1821100667"
+    serviceWorkerVersion: "3315180358"
   }
 });
